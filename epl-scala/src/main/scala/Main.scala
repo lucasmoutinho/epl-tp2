5aa2e7dd3374b3ae8c217eb0f9e68c16037fad38
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-04-09 16:30:52 -0300

    imported codigo do professor

---
+++ epl-scala/src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+import br.unb.cic.epl.Core
+import br.unb.cic.epl.Eval
+import br.unb.cic.epl.Add
+import br.unb.cic.epl.AddEval
+
+object Main extends App {
+  val lit100 = new Core.Literal(100) with Eval.Literal
+  val lit500 = new Core.Literal(500) with Eval.Literal
+
+  val sum = new AddEval.Add(lit100, lit500) 
+
+  println(sum.print())
+  println(sum.eval())
+  println(lit100.eval())
+}
