647008932b407277ffc39a41e8e47bafe5017120
Frederico Dib <fredericodib@cjr.org.br>
2018-04-09 17:01:25 -0300

    add duplo

--- epl-scala/src/main/scala/Main.scala
+++ epl-scala/src/main/scala/Main.scala
@@ -10,12 +10,15 @@ object Main extends App {
 
   val sum1 = new AddEval.Add(lit100, lit500)
   val sum2 = new AddEval.Add(lit100, sum1)
+  val sum3 = new AddEval.Add(sum2, sum1)
   val sub = new SubEval.Sub(lit100, lit500)
   val mult = new MultEval.Mult(lit100, lit500)
 
   println(sum1.print() + " = " + sum1.eval() + "\n")
 
   println(sum2.print() + " = " + sum2.eval() + "\n")
+
+  println(sum3.print() + " = " + sum3.eval() + "\n")
 
   println(sub.print() + " = " + sub.eval() + "\n")
 
