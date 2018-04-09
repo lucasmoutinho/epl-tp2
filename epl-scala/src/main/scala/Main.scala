54a150429c6bb9ea066ac380340fe86df0ee0d2b
Frederico Dib <fredericodib@cjr.org.br>
2018-04-09 16:36:56 -0300

    adicionando arquivos scala

--- epl-scala/src/main/scala/Main.scala
+++ epl-scala/src/main/scala/Main.scala
@@ -1,15 +1,20 @@
 import br.unb.cic.epl.Core
 import br.unb.cic.epl.Eval
-import br.unb.cic.epl.Add
 import br.unb.cic.epl.AddEval
+import br.unb.cic.epl.SubEval
+import br.unb.cic.epl.MultEval
 
 object Main extends App {
   val lit100 = new Core.Literal(100) with Eval.Literal
   val lit500 = new Core.Literal(500) with Eval.Literal
 
   val sum = new AddEval.Add(lit100, lit500) 
+  val sub = new SubEval.Sub(lit100, lit500)
+  val mult = new MultEval.Mult(lit100, lit500)
 
-  println(sum.print())
-  println(sum.eval())
-  println(lit100.eval())
+  println(sum.print() + " = " + sum.eval() + "\n")
+
+  println(sub.print() + " = " + sub.eval() + "\n")
+
+  println(mult.print() + " = " + mult.eval() + "\n")
 }
